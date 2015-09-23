53615dcfa0a518fe1ecc4553fb543fd5a3dcbb7e
pidashin <pidashin00003@yahoo.com.tw>
2015-09-23 11:44:24 +0800

    add test case: 一二三集各買了一本_價格應為270

--- PotterShoppingCart.TestsTests/HarryPotterCartTests.cs
+++ PotterShoppingCart.TestsTests/HarryPotterCartTests.cs
@@ -55,5 +55,22 @@ namespace PotterShoppingCart.Tests.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void 一二三集各買了一本_價格應為270()
+        {
+            //Arrange
+            var target = new HarryPotterCart();
+            books.Add(new Book { Serial = "HarryPotter", Episode = 1, price = 100 });
+            books.Add(new Book { Serial = "HarryPotter", Episode = 2, price = 100 });
+            books.Add(new Book { Serial = "HarryPotter", Episode = 3, price = 100 });
+            double expected = 270;
+
+            //Act
+            double actual = target.CountPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
